89b20e0942a2068dd37c5cd935d064ce1b71b779
Jin Wook Jeong <happygrammer.dev@gmail.com>
2018-06-07 02:18:16 +0900

    Update

--- ch11/generics/src/default/concat4.ts
+++ ch11/generics/src/default/concat4.ts
@@ -1,6 +1,6 @@
 function concat4<T extends string>(strs: T, strs2: T) {
     console.log(typeof strs, strs);
-    console.log(typeof strs2, strs2);
+    console.log(typeof strs2, strs2);    
     // return strs2 +strs2;
 }
 
